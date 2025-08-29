4342a2efaebb82357eafa2e89142e05e2bbae4db
egl-edu <egl-edu@proton.me>
2025-08-29 10:49:26 -0400

    Export exemple son et signal

---
+++ gd-example-audio-playback/source_son_bouton.gd
@@ -0,0 +1,11 @@
+extends AudioStreamPlayer
+
+
+func _on_button_down() -> void:
+	play()
+	pass # Replace with function body.
+	
+
+func _on_button_up() -> void:
+	play()
+	pass # Replace with function body.
